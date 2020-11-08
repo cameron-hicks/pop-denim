650268516457e2775c14814ab670ea230037810a
Cameron Baumgartner <cameron@Camerons-MacBook-Air.local>
2020-11-08 12:03:47 -0500

    outlined todos

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ const once = {
 window.addEventListener("pointermove", () => {
     $('#signupModal').modal("show");
 }, once);
+
+
+// Make an array of objects. Each object is a product to show on the shop.html page. Properties include product name, picture url, colors (light/med/dark wash), etc.
+// Iterate through the array, using createElement to create a Bootstrap card component for each. On the card, include the picture, the product name, and circular images showing a preview of each of the colors available.
+// Consider breaking down the card initialization function into smaller parts, eg, an initializer function for each of the color swatches which makes a styled <img> element to be appended to the card.
